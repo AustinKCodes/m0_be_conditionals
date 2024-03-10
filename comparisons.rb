56b65dccd8acd922b0b0aac8a736f8be73606ec3
Austin Kenny <austinkennywork@gmail.com>
2024-03-10 10:58:24 -0400

    Finished Sections 2-3

--- comparisons.rb
+++ comparisons.rb
@@ -61,21 +61,23 @@ puts "Is number_students less than or equal to 21", number_students <= 21
 
 puts 4 < 9
 #YOU DO: Explain.
-# 
+# This is evaluating if 4 is less than 9. This will print out true
 books = 3
+# This will not print out anything due to their being no 'puts' statement. It is evaluating if books is equal to 3. This would print out as false
 puts 4 < books
+#This is evaluating if the 4 is less than books. This will print out true
 # YOU DO: Explain.
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-
+# This is evaluating if friends is greater than siblings. Since friends is valued at 6 and siblings is valued at 2 this will print out as true
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+# This is evaluating if the amount of attendees is not equal to the meals. This will print out as true due to them not being equal
 
 #-------------------
 # PART 3: Logical Operators
@@ -97,16 +99,16 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
-
+puts loves_to_play && age < 2
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: I don't really understand this question but, will try my best anyways. My final line of code evaluated to true because the dog loved to play and was under the age of 2.
